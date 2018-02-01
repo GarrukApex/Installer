cea2352f339ca16520a4818f4da5ee6f066f1a48
Jacob <GarrukApex@users.noreply.github.com>
2018-01-31 16:08:16 -0800

    Add files via upload

---
+++ Installer.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Made by GarrukApex, 2018
+
+echo "This script is designed to install software that doesn't come by default on most Raspberry Pis/Bare bones linux distros. This is software that I find useful. You might not find the same software useful. Feel free to modify this script to your liking." 
+#Uncomment the lines below to install software that doesn't install by default. 
+read -p "Would you like to continue or modify the script before running?: [Y/N]" answer
+
+case $answer in 
+	[Yy]* ) 
+		#Install the basics
+		sudo apt-get update && sudo apt-get upgrade -y
+
+		sudo apt-get install nmap vim openvpn git netcat arp-scan -y
+
+		#Install OpenVPN Server
+		#wget https://git.io/vpn -O openvpn.sh && bash openvpn.sh
+
+		#Install BitchX, a terminal IRC Client
+		#sudo apt-get install bitchx
+
+		#Install HexChat, a GUI based IRC Client
+		# sudo apt-get install hexchat
+
+		#Pentesting tools
+		#sudo apt-get install aircrack-ng ddrescue sqlmap exploitdb tshark -y
+
+			# install metasploit
+			# sudo add-apt-repository -y ppa:webupd8team/java
+			# sudo apt-get -y install oracle-java8-installer
+			# sudo apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev vncviewer libyaml-dev curl zlib1g-dev
+			# curl -sSL https://rvm.io/mpapis.asc | gpg2 --import - & curl -L https://get.rvm.io | bash -s stable & source ~/.rvm/scripts/rvm & echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc & source ~/.bashrc & RUBYVERSION=$(wget https://raw.githubusercontent.com/rapid7/metasploit-framework/master/.ruby-version -q -O -  & ) & rvm install $RUBYVERSION & rvm use $RUBYVERSION --default & ruby -v
+			# sudo -s & su postgres & createuser msf -P -S -R -D & createdb -O msf msf & exit & exit
+ 		# 	cd /opt & sudo git clone https://github.com/rapid7/metasploit-framework.git & sudo chown -R `whoami` /opt/metasploit-framework & cd metasploit-framework & rvm --default use ruby-${RUByVERSION}@metasploit-framework & gem install bundler & bundle install & sudo bash -c 'for MSF in $(ls msf*); do ln -s /opt/metasploit-framework/$MSF /usr/local/bin/$MSF;done'
+ 		# 	echo "Run 'msfconsole' in order to start metasploit"
+
+
+
+
+		;;
+
+
+
+	* ) echo "Exiting now" && exit;;
+
+esac
