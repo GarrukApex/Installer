56f5007c70a0977fa02ea68614521b2221203c86
PwnKitteh <jacobbokor@gmail.com>
2018-02-09 19:32:03 -0800

    Changes to Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -12,7 +12,7 @@ case $answer in
 		sudo apt-get update && sudo apt-get upgrade -y
 
 		sudo apt-get install nmap vim openvpn git netcat arp-scan reaver tcpdump -y
-		
+
 		#Installing Ettercap
 		sudo apt-get install debhelper cmake bison flex libgtk2.0-dev libltdl3-dev libncurses-dev libncurses5-dev\ libnet1-dev libpcap-dev libpcre3-dev libssl-dev libcurl4-openssl-dev ghostscript ettercap-text-only
 		
@@ -29,11 +29,11 @@ case $answer in
 		#Pentesting tools
 		#sudo apt-get install aircrack-ng ddrescue sqlmap exploitdb tshark -y
 
-			# install metasploit
-			# sudo add-apt-repository -y ppa:webupd8team/java
-			# sudo apt-get -y install oracle-java8-installer
-			# sudo apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev vncviewer libyaml-dev curl zlib1g-dev
-			# curl -sSL https://rvm.io/mpapis.asc | gpg2 --import - & curl -L https://get.rvm.io | bash -s stable & source ~/.rvm/scripts/rvm & echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc & source ~/.bashrc & RUBYVERSION=$(wget https://raw.githubusercontent.com/rapid7/metasploit-framework/master/.ruby-version -q -O -  & ) & rvm install $RUBYVERSION & rvm use $RUBYVERSION --default & ruby -v
+			#install metasploit
+			#sudo add-apt-repository -y ppa:webupd8team/java
+			#sudo apt-get -y install oracle-java8-installer
+			#sudo apt-get install build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev vncviewer libyaml-dev curl zlib1g-dev
+			#curl -sSL https://rvm.io/mpapis.asc | gpg2 --import - & curl -L https://get.rvm.io | bash -s stable & source ~/.rvm/scripts/rvm & echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc & source ~/.bashrc & RUBYVERSION=$(wget https://raw.githubusercontent.com/rapid7/metasploit-framework/master/.ruby-version -q -O -  & ) & rvm install $RUBYVERSION & rvm use $RUBYVERSION --default & ruby -v
 			# sudo -s & su postgres & createuser msf -P -S -R -D & createdb -O msf msf & exit & exit
  		# 	cd /opt & sudo git clone https://github.com/rapid7/metasploit-framework.git & sudo chown -R `whoami` /opt/metasploit-framework & cd metasploit-framework & rvm --default use ruby-${RUByVERSION}@metasploit-framework & gem install bundler & bundle install & sudo bash -c 'for MSF in $(ls msf*); do ln -s /opt/metasploit-framework/$MSF /usr/local/bin/$MSF;done'
  		# 	echo "Run 'msfconsole' in order to start metasploit"
